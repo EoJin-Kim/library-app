2ced34993e8b318724e32a23d81bc70afca21c12
EoJin-Kim <linexy@naver.com>
2022-10-11 11:16:01 +0900

    Feat: 테스트 코드 작성 완료

--- src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
+++ src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
@@ -1,6 +1,7 @@
 package com.group.libraryapp.domain.user.loanhistory;
 
 import com.group.libraryapp.domain.user.User;
+import org.jetbrains.annotations.NotNull;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -33,6 +34,7 @@ public class UserLoanHistory {
     this.isReturn = isReturn;
   }
 
+  @NotNull
   public String getBookName() {
     return this.bookName;
   }
@@ -41,4 +43,33 @@ public class UserLoanHistory {
     this.isReturn = true;
   }
 
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  @NotNull
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  public void setBookName(String bookName) {
+    this.bookName = bookName;
+  }
+
+  @NotNull
+  public boolean isReturn() {
+    return isReturn;
+  }
+
+  public void setReturn(boolean aReturn) {
+    isReturn = aReturn;
+  }
 }
--- src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -1,10 +1,76 @@
 package com.group.libraryapp.service.user
 
+import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
 
+@SpringBootTest
 class UserServiceTest @Autowired constructor(
     private val userRepository :UserRepository,
     private val userService : UserService
 ) {
+
+    @AfterEach
+    fun clean(){
+        userRepository.deleteAll()
+    }
+
+    @Test
+    @DisplayName("유저 조회")
+    fun saveUserTest(){
+        val request = UserCreateRequest("김어진", null)
+
+        userService.saveUser(request)
+
+        val results = userRepository.findAll()
+
+        assertThat(results).hasSize(1)
+        assertThat(results[0].name).isEqualTo("김어진")
+        assertThat(results[0].age).isNull()
+    }
+
+    @Test
+    fun getUsersTest(){
+
+        userRepository.saveAll(listOf(
+            User("A",20),
+            User("B",null)
+        ))
+
+        val results = userService.getUsers()
+
+        assertThat(results).hasSize(2)
+        assertThat(results).extracting("name").containsExactlyInAnyOrder("A","B")
+        assertThat(results).extracting("age").containsExactlyInAnyOrder(20,null)
+    }
+
+    @Test
+    fun updateUserNameTest(){
+        val savedUser = userRepository.save(User("A",null))
+        val request = UserUpdateRequest(savedUser.id,"B")
+
+
+        userService.updateUserName(request)
+
+        val result  = userRepository.findAll()[0]
+        assertThat(result.name).isEqualTo("B")
+    }
+
+    @Test
+    fun deleteUserTest(){
+        userRepository.save(User("A",null))
+        userService.deleteUser("A")
+
+        assertThat(userRepository.findAll()).isEmpty()
+    }
+
+
 }
